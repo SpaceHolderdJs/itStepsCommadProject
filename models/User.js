e72dfeaac2e1ee73a5641ae992cc3a6e875eaa3f
Igor <62031924+SpaceHolderdJs@users.noreply.github.com>
2021-04-12 19:39:15 +0300

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  posts: {
+    type: Array,
+  },
+  registrationDate: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("Main", userSchema);
